980c5e7d6ef8c5f2b07cc4e8f65bad9884ed16a3
Graidy27 <graidyits@gmail.com>
2023-06-17 18:27:16 +0700

    Add Tugas 2

---
+++ Tugas 2/.ipynb_checkpoints/server-checkpoint.py
@@ -0,0 +1,60 @@
+from socket import *
+import socket
+import threading
+import logging
+import time
+import sys
+from datetime import datetime
+
+class ProcessTheClient(threading.Thread):
+    def __init__(self,connection,address):
+        self.connection = connection
+        self.address = address
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            data = self.connection.recv(32)
+            if data:
+                if data.startswith(b"TIME") and data.endswith(b"\r\n"):
+                    waktuSekarang = datetime.now()
+                    # print(waktuSekarang)
+                    
+                    formatWaktu = waktuSekarang.strftime("%H:%M:%S")
+                    # print(formatWaktu)
+                    
+                    response = f'JAM {formatWaktu}\r\n'
+                    print(f'[DATA SEND TO CLIENT] {response}')
+                    
+                    response = bytes(response, 'utf-8')
+                    
+                    self.connection.sendall(response)
+            else:
+                break
+        self.connection.close()
+
+class Server(threading.Thread):
+    print("[STATUS] Server is running")
+    def __init__(self):
+        self.the_clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        self.my_socket.bind(('0.0.0.0',45000))
+        self.my_socket.listen(1)
+        while True:
+            self.connection, self.client_address = self.my_socket.accept()
+            logging.warning(f"connection from {self.client_address}")
+            
+            clt = ProcessTheClient(self.connection, self.client_address)
+            clt.start()
+            self.the_clients.append(clt)
+
+
+def main():
+    svr = Server()
+    svr.start()
+
+if __name__=="__main__":
+    main()
